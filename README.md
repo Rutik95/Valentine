d49df2a223c7b05aeedd9a369bce55dbe0f92397
Rutik Tarekar <rutiktarekar95@gmail.com>
2026-02-01 19:53:08 +0530

    Initial commit for Valentines project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Valentines
+
+A simple Valentine's Day web page.
+
+## How to run
+
+Open `index.html` in your web browser.
---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('loveButton').addEventListener('click', function() {
+    alert('You are loved! ❤️');
+});
